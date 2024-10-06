522db6c97d3a8a1b41de1d9ec613a192ac5ff884
seanpden <seanden522@gmail.com>
2024-10-06 18:13:45 -0500

    feat: basic keymaps and options

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set({ "i" }, "jj", "<esc>")
+vim.keymap.set({ "i" }, "jk", "<esc>")
+vim.keymap.set({ "n" }, "|", ":vsplit<CR>")
+vim.keymap.set({ "n" }, "<leader>;", ":Dashboard<CR>")
