a0b21060b24ddb3d85718683f2d4501481761d28
seanpden <seanden522@gmail.com>
2024-10-06 20:17:23 -0500

    feat: enabled comment plugin and hotkeys

---
+++ .config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,16 @@
+return {
+  -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
+  "numToStr/Comment.nvim",
+  opts = {
+    -- add any options here
+    toggler = {
+      line = "<leader>/",
+    },
+    opleader = {
+      line = "<leader>/",
+    },
+    mappings = {
+      extra = false,
+    },
+  },
+}
