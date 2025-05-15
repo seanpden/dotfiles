6f1104c451be4672d24084f98e5b9c92a259a56a
Sean P Denney <seanden522@gmail.com>
2025-05-15 09:25:25 -0500

    fix: 🐛 handles different PDF viewer on mac vs windows

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -1,8 +1,14 @@
 return {
   "lervag/vimtex",
   init = function()
-    vim.g.vimtex_view_general_viewer = "sumatraPDF"
-    vim.g.vimtex_view_general_options = "-reuse-instance @pdf"
-    vim.g.vimtex_view_general_options_latexmk = "-reuse-instance"
+    local user_os = jit.os
+
+    if user_os == "OSX" then
+      vim.g.vimtex_view_general_viewer = "zathura"
+    else
+      vim.g.vimtex_view_general_viewer = "sumatraPDF"
+      vim.g.vimtex_view_general_options = "-reuse-instance @pdf"
+      vim.g.vimtex_view_general_options_latexmk = "-reuse-instance"
+    end
   end,
 }
